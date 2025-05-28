a5a5f48e04878baaff8da668780e2bd8cd9fee30
Elias Arriola <earrio@uw.edu>
2025-05-28 12:01:36 -0700

    fixing deployment errors

--- src/views/books/books-single.tsx
+++ src/views/books/books-single.tsx
@@ -1,25 +1,25 @@
-'use client';
+// 'use client';
 
-import { useParams, notFound } from 'next/navigation';
-import { Container } from '@mui/material';
-import { IBook } from 'types/books';
-import { mockBooks } from 'utils/mockBooks';
-import BookItem from 'components/BookItem';
+// import { useParams, notFound } from 'next/navigation';
+// import { Container } from '@mui/material';
+// import { IBook } from 'types/books';
+// import { mockBooks } from 'utils/mockBooks';
+// import BookItem from 'components/BookItem';
 
-export default function SingleBookPage() {
-  const params = useParams();
-  const isbn13 = params?.id as string;
+// export default function SingleBookPage() {
+//   const params = useParams();
+//   const isbn13 = params?.id as string;
 
-  const book: IBook | undefined = mockBooks.find((b) => b.isbn13 === isbn13);
+//   const book: IBook | undefined = mockBooks.find((b) => b.isbn13 === isbn13);
 
-  if (!book) {
-    notFound();
-    return null;
-  }
+//   if (!book) {
+//     notFound();
+//     return null;
+//   }
 
-  return (
-    <Container maxWidth="md" sx={{ mt: 4 }}>
-      <BookItem book={book} />
-    </Container>
-  );
-}
+//   return (
+//     <Container maxWidth="md" sx={{ mt: 4 }}>
+//       <BookItem book={book} />
+//     </Container>
+//   );
+// }
