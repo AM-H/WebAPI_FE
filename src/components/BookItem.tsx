f422e75f664f4320c83209dcc4f28b343ba9c33a
alimohamed <alisokor018@gmail.com>
2025-05-30 01:30:49 -0700

    update single book ui

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,7 +1,9 @@
-import { Avatar, Typography, Card, CardContent, Box } from '@mui/material';
-import BookRating from 'components/BookRating';
+import { Avatar, Typography, Card, CardContent, Box, Rating } from '@mui/material';
 import { mockBooks } from 'utils/mockBooks';
 import { Book } from 'types/books';
+
+import DeleteItem from './DeleteItem';
+import BookRating from './BookRating';
 
 export default function BookItem({ id }: { id: number }) {
   const book: Book | undefined = mockBooks.find((b) => b.id === id);
@@ -12,30 +14,28 @@ export default function BookItem({ id }: { id: number }) {
 
   return (
     <Card sx={{ p: 4, maxWidth: 600, mx: 'auto', mt: 4 }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 3 }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: 500, height: 500, alignItems: 'center', mb: 3 }}>
         <Typography variant="h4" gutterBottom>
           {book.title}
         </Typography>
-        <Typography variant="subtitle1" color="text.secondary" gutterBottom>
-          {book.author}
+        <Typography variant="subtitle1" gutterBottom>
+          By: {book.author}
         </Typography>
-        <Avatar
-          alt={book.title}
-          src={book.image}
-          variant="square"
-          sx={{ width: 250, height: 375, my: 2 }}
-        />
+        <Typography variant="subtitle1" sx={{ mb: 2 }}>
+          Publication Year: 1990
+        </Typography>
+        <Avatar alt={book.title} src={book.image} variant="square" sx={{ width: 250, height: 375, my: 2 }} />
       </Box>
       <CardContent>
-        <Typography variant="body1" sx={{ mb: 2 }}>
-          {book.description}
-        </Typography>
-        <BookRating rating={book.ratings} />
-        <Typography variant="subtitle1" sx={{ mt: 3 }}>
-          Leave a rating
-        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Rating value={book.ratings.average} readOnly precision={0.5} />
+            <Typography sx={{ ml: 1 }}>{book.ratings.average.toFixed(1)}</Typography>
+          </Box>
+          <DeleteItem></DeleteItem>
+        </Box>
+        <BookRating></BookRating>
       </CardContent>
     </Card>
   );
 }
-
--- src/components/BookRating.tsx
+++ src/components/BookRating.tsx
@@ -1,33 +1,31 @@
-import * as React from 'react';
-import Rating from '@mui/material/Rating';
-import Box from '@mui/material/Box';
-import StarIcon from '@mui/icons-material/Star';
-import {IRatings} from '../types/ratings'
+import { Dialog, Button, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
 
+import { useState } from 'react';
 
-
-export default function BookRating({rating} :{rating : IRatings}) {
-  const [value, setValue] = React.useState<number | null>(rating.average);
-  const [hover, setHover] = React.useState(-1);
-
+export default function BookRating() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <Box sx={{ width: 200, display: 'flex', alignItems: 'center' }}>
-      <Rating
-        name="hover-feedback"
-        value={value}
-        precision={0.5}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        onChangeActive={(event, newHover) => {
-          setHover(newHover);
-        }}
-        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-      />
-      {value !== null && (
-        <Box sx={{ ml: 2 }}>{hover !== -1 ? hover : value.toFixed(1)}</Box>
-        )}
-    </Box>
+    <>
+      <Button onClick={handleOpen}>Update rating</Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="Update Review" aria-describedby="Set up a new review">
+        <DialogTitle>Update Rating</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Enter amount of 1 star ratings:</DialogContentText>
+          <TextField autoFocus required margin="dense" id="name" name="rating1" label="0" type="number" fullWidth variant="standard" />
+          <DialogContentText>Enter amount of 2 star ratings:</DialogContentText>
+          <TextField autoFocus required margin="dense" id="name" name="rating2" label="0" type="number" fullWidth variant="standard" />
+          <DialogContentText>Enter amount of 3 star ratings:</DialogContentText>
+          <TextField autoFocus required margin="dense" id="name" name="rating3" label="0" type="number" fullWidth variant="standard" />
+          <DialogContentText>Enter amount of 4 star ratings:</DialogContentText>
+          <TextField autoFocus required margin="dense" id="name" name="rating4" label="0" type="number" fullWidth variant="standard" />
+          <DialogContentText>Enter amount of 5 star ratings:</DialogContentText>
+          <TextField autoFocus required margin="dense" id="name" name="rating5" label="0" type="number" fullWidth variant="standard" />
+          <Button onClick={handleClose}>Enter</Button>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
